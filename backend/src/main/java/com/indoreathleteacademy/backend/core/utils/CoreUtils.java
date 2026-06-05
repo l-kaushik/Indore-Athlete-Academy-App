8d51a04608ddf5003bb50d041984755888765455
Lokesh Kaushik <contact.lokeshkaushik@gmail.com>
2026-06-05 23:18:30 +0530

    Feat: Implemented getCurrentUser method in CoreUtils.
    - Provides UserAuth entity to other packages.

--- backend/src/main/java/com/indoreathleteacademy/backend/core/utils/CoreUtils.java
+++ backend/src/main/java/com/indoreathleteacademy/backend/core/utils/CoreUtils.java
@@ -2,14 +2,28 @@ package com.indoreathleteacademy.backend.core.utils;
 
 import com.fasterxml.uuid.Generators;
 import com.fasterxml.uuid.impl.TimeBasedEpochGenerator;
+import com.indoreathleteacademy.backend.auth.entities.UserAuth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public class CoreUtils {
+    @Autowired
+    private static final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
     private static final TimeBasedEpochGenerator UUID_V7_GENERATOR = Generators.timeBasedEpochGenerator();
 
     public static UUID generateUuidV7() {
         return UUID_V7_GENERATOR.generate();
+    }
+
+    public static UserAuth getCurrentUser() {
+        if(authentication != null && authentication.getPrincipal() instanceof UserDetails user) {
+            return (UserAuth) user;
+        }
+        throw new IllegalStateException("Authenticated principal is not of type UserAuth");
     }
 }
